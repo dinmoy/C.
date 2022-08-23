e0392ad60c954a23ddba74e42807d04ba50a4b3f
dinmoy <111869216+dinmoy@users.noreply.github.com>
2022-08-24 08:48:23 +0900

    Add files via upload

---
+++ Test03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+    <제어문자(Escape Sequence) >
+    \n : new line, 줄바꿈
+    \t : tab
+    \\ : 문자그대로의 '\'
+    \b : back space
+    \0 : nu11
+    \' : 문자 그대로의 '
+    \" : 문자 그대로의 "
+*/
+int main(){
+
+    printf("Hello!C \n"); 
+    printf("Hello!C++ \t");
+    printf("Hello!Python \\");
+    printf("Hello!JAVA \b");
+    printf("Hello!Per1 \0");
+    printf("피카츄가\'피카피카\'라고말했다")
+    printf("피카츄가\"피카피카\"라고말했다")
+
+         return 0;
+}
