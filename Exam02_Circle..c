a5505f7cef74db1b1eef56eb53ea4f75c59cdc12
Hyojin Lim <111869216+dinmoy@users.noreply.github.com>
2022-09-23 08:53:26 +0900

    원의 넓이와 둘레

---
+++ Exam02_Circle..c
@@ -0,0 +1,22 @@
+//원의 반지름을 입력받아 원의 넓이와 둘레를 구하는 프로그램
+#include <stdio.h>
+int main()
+{
+	//변수 선언
+	int radius=0;	//반지름
+	float area=0.0, round=0.0, pi = 3.141592;
+
+	printf("** 원의 넓이와 둘레 ** \n");
+	//내장함함수-printf()
+	printf("반지름에 대한 입력 값: ");
+	scanf_s("%d", &radius);
+
+
+
+	return 0;
+
+}
+//사용자 정의 함수
+void choi() {
+
+}
