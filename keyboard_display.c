9e02c33ee1cffabd4d2960ff6af61dbbd6590c05
Hyojin Lim <111869216+dinmoy@users.noreply.github.com>
2023-01-08 23:56:18 +0900

    keyboardgame_display

---
+++ keyboard_display.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void display_keyboard(void) {
+	printf("영문 소문자 자판위치\n");
+	printf("-------------------------\n");
+	printf("q w e r t y u i o p\n");
+	printf("a s d f g h j k l\n");
+	printf("z x c v b n m\n");
+	printf("-------------------------\n");
+}
